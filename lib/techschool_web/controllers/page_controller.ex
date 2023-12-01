0316f8563b2976acc369c366e03dd75c403f58ad
Daniel Bergholz <bergholz.daniel@gmail.com>
2023-12-01 10:03:12 -0300

    Add home page

--- lib/techschool_web/controllers/page_controller.ex
+++ lib/techschool_web/controllers/page_controller.ex
@@ -2,8 +2,6 @@ defmodule TechschoolWeb.PageController do
   use TechschoolWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
+    render(conn, :home)
   end
 end
