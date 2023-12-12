79d1b5e6b34ac748f0456828b351790c4fb8dd51
Daniel Bergholz <bergholz.daniel@gmail.com>
2023-12-12 14:15:32 -0300

    Add `SetLocale` plug to the router

--- lib/techschool_web/router.ex
+++ lib/techschool_web/router.ex
@@ -8,6 +8,7 @@ defmodule TechschoolWeb.Router do
     plug :put_root_layout, html: {TechschoolWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug TechschoolWeb.SetLocale
   end
 
   pipeline :api do
