842a02cb7bee549eba7703f43538ffb4ea1db2c2
Daniel Bergholz <bergholz.daniel@gmail.com>
2023-12-18 11:57:50 -0300

    Configure mix and formatter for priv/repo/support
    This folder will contain some EX code to seed the database, so the
    formatter and mix must work properly inside it

--- mix.exs
+++ mix.exs
@@ -25,6 +25,7 @@ defmodule Techschool.MixProject do
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(:dev), do: ["lib", "priv/repo/support"]
   defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.
