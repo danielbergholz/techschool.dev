36c5858019f80d27b40ce2dd5b89d380501bfffb
Daniel Bergholz <bergholz.daniel@gmail.com>
2024-01-12 17:39:22 -0300

    Create seed_courses helper function

--- priv/repo/support/seeds_helper.ex
+++ priv/repo/support/seeds_helper.ex
@@ -1,5 +1,5 @@
 defmodule Techschool.SeedsHelper do
-  alias Techschool.{Languages, Frameworks, Channels}
+  alias Techschool.{Languages, Frameworks, Channels, Courses}
 
   def seed_languages() do
     "priv/repo/data/languages.json"
@@ -42,5 +42,26 @@ defmodule Techschool.SeedsHelper do
       image_url: channel[:image_url],
       youtube_channel_id: channel[:youtube_channel_id]
     })
+  end
+
+  def seed_courses() do
+    "priv/repo/data/courses.json"
+    |> File.read!()
+    |> Jason.decode!(keys: :atoms)
+    |> Enum.each(&insert_course/1)
+  end
+
+  defp insert_course(course) do
+    Courses.create_course!(
+      %{
+        name: course[:name],
+        type: course[:type],
+        locale: course[:locale],
+        image_url: course[:image_url],
+        published_at: course[:published_at],
+        youtube_course_id: course[:youtube_course_id]
+      },
+      course[:youtube_channel_id]
+    )
   end
 end
