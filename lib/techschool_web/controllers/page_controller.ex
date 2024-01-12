e4572c10066b5d1c86e219638f9b814c1285f41d
Daniel Bergholz <bergholz.daniel@gmail.com>
2024-01-11 22:19:26 -0300

    Add 2 hours of cache to home page

--- lib/techschool_web/controllers/page_controller.ex
+++ lib/techschool_web/controllers/page_controller.ex
@@ -1,7 +1,11 @@
 defmodule TechschoolWeb.PageController do
   use TechschoolWeb, :controller
 
+  @two_hours 2 * 60 * 60
+
   def home(conn, _params) do
-    render(conn, :home)
+    conn
+    |> put_resp_header("cache-control", "public, max-age=#{@two_hours}")
+    |> render(:home)
   end
 end
