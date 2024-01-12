917c3f2ce43e309544f387df3b581f4ad46d8ceb
Daniel Bergholz <bergholz.daniel@gmail.com>
2024-01-12 15:04:17 -0300

    Remove cache from home page
    There were a couple of bugs happening due to translation

--- lib/techschool_web/controllers/page_controller.ex
+++ lib/techschool_web/controllers/page_controller.ex
@@ -1,11 +1,7 @@
 defmodule TechschoolWeb.PageController do
   use TechschoolWeb, :controller
 
-  @two_hours 2 * 60 * 60
-
   def home(conn, _params) do
-    conn
-    |> put_resp_header("cache-control", "public, max-age=#{@two_hours}")
-    |> render(:home)
+    render(conn, :home)
   end
 end
