740bdca0fd8719150d6dab57befdba238f7e86ce
Daniel Bergholz <bergholz.daniel@gmail.com>
2024-01-13 18:21:50 -0300

    Add cache to courses page

--- lib/techschool_web/controllers/course_controller.ex
+++ lib/techschool_web/controllers/course_controller.ex
@@ -3,10 +3,13 @@ defmodule TechschoolWeb.CourseController do
 
   alias Techschool.Courses
 
+  @one_hour 1 * 60 * 60
+
   def index(conn, _params) do
     courses = Courses.list_courses()
 
     conn
+    |> put_resp_header("cache-control", "public, max-age=#{@one_hour}")
     |> assign(:page_title, gettext("Courses") <> " | TechSchool")
     |> assign(:courses, courses)
     |> render(:index)
