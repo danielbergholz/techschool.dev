6cebea5178966dd770a840c5671a58268185e597
Daniel Bergholz <bergholz.daniel@gmail.com>
2024-01-15 10:58:01 -0300

    Test if there is caching on the home page

--- test/techschool_web/controllers/page_controller_test.exs
+++ test/techschool_web/controllers/page_controller_test.exs
@@ -1,8 +1,16 @@
 defmodule TechschoolWeb.PageControllerTest do
   use TechschoolWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "TechSchool"
+  describe "GET /" do
+    test "Returns 200 status code", %{conn: conn} do
+      conn = get(conn, ~p"/")
+      assert html_response(conn, 200) =~ "TechSchool"
+    end
+
+    test "Has a non private cache-control header", %{conn: conn} do
+      conn = get(conn, ~p"/")
+      assert {"cache-control", cache_value} = List.keyfind!(conn.resp_headers, "cache-control", 0)
+      refute String.contains?(cache_value, "private")
+    end
   end
 end
