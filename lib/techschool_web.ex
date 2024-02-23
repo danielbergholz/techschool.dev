b73970265fa3c8aa70f32beece2325aff2a17751
Daniel Bergholz <bergholz.daniel@gmail.com>
2024-02-22 23:08:33 -0300

    Create `sitemap.xml`

--- lib/techschool_web.ex
+++ lib/techschool_web.ex
@@ -17,7 +17,7 @@ defmodule TechschoolWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt sitemap.xml)
 
   def router do
     quote do
