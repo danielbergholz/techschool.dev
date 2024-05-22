0c7a6ce15f28ec5557d7b47bd9a554b82e0944c1
Daniel Bergholz <bergholz.daniel@gmail.com>
2024-05-22 16:35:12 -0300

    Install `cachex` package (to cache API requests)
    And avoid being rate limited

--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule Techschool.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.2"},
-      {:req, "~> 0.4.0"}
+      {:req, "~> 0.4.0"},
+      {:cachex, "~> 3.6"}
     ]
   end
 
