d3a360798827f5a41176357305ec9f63d2811896
Daniel Bergholz <bergholz.daniel@gmail.com>
2024-05-22 15:53:38 -0300

    Install `req` package (for HTTP requests)

--- mix.exs
+++ mix.exs
@@ -57,7 +57,8 @@ defmodule Techschool.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:req, "~> 0.4.0"}
     ]
   end
 
