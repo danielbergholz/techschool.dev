d2239b6877655de474f8b288556d5d864088ead7
Daniel Bergholz <bergholz.daniel@gmail.com>
2024-06-01 13:28:57 -0300

    Update seed functions to use "!"

--- lib/techschool/helpers/seed.ex
+++ lib/techschool/helpers/seed.ex
@@ -13,81 +13,81 @@ defmodule Techschool.Helpers.Seed do
 
   def call(data_folder_path \\ "priv/repo/data") do
     Techschool.Helpers.ResetDb.call()
-    seed_languages(data_folder_path)
-    seed_frameworks(data_folder_path)
-    seed_tools(data_folder_path)
-    seed_fundamentals(data_folder_path)
-    seed_lessons(data_folder_path)
-    seed_bootcamps(data_folder_path)
-    seed_channels(data_folder_path)
-    seed_courses(data_folder_path)
-    seed_platforms(data_folder_path)
+    seed_languages!(data_folder_path)
+    seed_frameworks!(data_folder_path)
+    seed_tools!(data_folder_path)
+    seed_fundamentals!(data_folder_path)
+    seed_lessons!(data_folder_path)
+    seed_bootcamps!(data_folder_path)
+    seed_channels!(data_folder_path)
+    seed_courses!(data_folder_path)
+    seed_platforms!(data_folder_path)
   end
 
-  defp seed_languages(data_folder_path) do
+  defp seed_languages!(data_folder_path) do
     "#{data_folder_path}/languages.json"
     |> File.read!()
     |> Jason.decode!(keys: :atoms)
     |> Enum.each(&Languages.create_language!(&1))
   end
 
-  defp seed_frameworks(data_folder_path) do
+  defp seed_frameworks!(data_folder_path) do
     "#{data_folder_path}/frameworks.json"
     |> File.read!()
     |> Jason.decode!(keys: :atoms)
     |> Enum.each(&Frameworks.create_framework!(&1))
   end
 
-  defp seed_tools(data_folder_path) do
+  defp seed_tools!(data_folder_path) do
     "#{data_folder_path}/tools.json"
     |> File.read!()
     |> Jason.decode!(keys: :atoms)
     |> Enum.each(&Tools.create_tool!(&1))
   end
 
-  defp seed_fundamentals(data_folder_path) do
+  defp seed_fundamentals!(data_folder_path) do
     "#{data_folder_path}/fundamentals.json"
     |> File.read!()
     |> Jason.decode!(keys: :atoms)
     |> Enum.each(&Fundamentals.create_fundamental!(&1))
   end
 
-  defp seed_bootcamps(data_folder_path) do
+  defp seed_bootcamps!(data_folder_path) do
     "#{data_folder_path}/bootcamps.json"
     |> File.read!()
     |> Jason.decode!(keys: :atoms)
-    |> Enum.each(&insert_bootcamp/1)
+    |> Enum.each(&insert_bootcamp!/1)
   end
 
-  defp insert_bootcamp(bootcamp) do
+  defp insert_bootcamp!(bootcamp) do
     Bootcamps.create_bootcamp!(
       bootcamp,
       lesson_names: bootcamp[:lesson_names]
     )
   end
 
-  defp seed_lessons(data_folder_path) do
+  defp seed_lessons!(data_folder_path) do
     "#{data_folder_path}/lessons.json"
     |> File.read!()
     |> Jason.decode!(keys: :atoms)
     |> Enum.each(&Lessons.create_lesson!(&1))
   end
 
-  defp seed_channels(data_folder_path) do
+  defp seed_channels!(data_folder_path) do
     "#{data_folder_path}/channels.json"
     |> File.read!()
     |> Jason.decode!(keys: :atoms)
     |> Enum.each(&Channels.create_channel!(&1))
   end
 
-  defp seed_courses(data_folder_path) do
+  defp seed_courses!(data_folder_path) do
     "#{data_folder_path}/courses.json"
     |> File.read!()
     |> Jason.decode!(keys: :atoms)
-    |> Enum.each(&insert_course/1)
+    |> Enum.each(&insert_course!/1)
   end
 
-  defp insert_course(course) do
+  defp insert_course!(course) do
     Courses.create_course!(
       course[:youtube_channel_id],
       course,
@@ -98,7 +98,7 @@ defmodule Techschool.Helpers.Seed do
     )
   end
 
-  defp seed_platforms(data_folder_path) do
+  defp seed_platforms!(data_folder_path) do
     "#{data_folder_path}/platforms.json"
     |> File.read!()
     |> Jason.decode!(keys: :atoms)
