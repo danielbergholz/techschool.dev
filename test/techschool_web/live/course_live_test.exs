7e883e3b77220d0bd4fb8818289378b7634c0a80
Daniel Bergholz <bergholz.daniel@gmail.com>
2024-06-03 12:08:35 -0300

    Add platforms to courses page test

--- test/techschool_web/live/course_live_test.exs
+++ test/techschool_web/live/course_live_test.exs
@@ -2,15 +2,17 @@ defmodule TechschoolWeb.CourseLiveTest do
   use TechschoolWeb.ConnCase
 
   import Phoenix.LiveViewTest
-  import Techschool.{CoursesFixtures, ChannelsFixtures}
+  import Techschool.{CoursesFixtures, ChannelsFixtures, PlatformsFixtures}
 
   describe "GET /:locale/courses" do
-    test "lists all courses", %{conn: conn} do
+    test "lists all courses and platforms", %{conn: conn} do
       channel = channel_fixture()
       course = course_fixture(channel.youtube_channel_id)
+      platform = platform_fixture()
 
       assert {:ok, _index_live, html} = live(conn, ~p"/en/courses")
       assert html =~ course.name
+      assert html =~ platform.name
     end
   end
 
