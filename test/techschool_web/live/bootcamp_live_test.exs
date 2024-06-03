fb1d55a9584108cb5f29f2acf168335c49986926
Daniel Bergholz <bergholz.daniel@gmail.com>
2024-06-03 11:52:33 -0300

    Add tests for lessons page

--- test/techschool_web/live/bootcamp_live_test.exs
+++ test/techschool_web/live/bootcamp_live_test.exs
@@ -2,10 +2,26 @@ defmodule TechschoolWeb.BootcampLiveTest do
   use TechschoolWeb.ConnCase
 
   import Phoenix.LiveViewTest
+  import Techschool.BootcampsFixtures
+  import Techschool.LessonsFixtures
 
   describe "GET /:locale/bootcamps" do
-    test "Returns 200 status code", %{conn: conn} do
-      assert {:ok, _bootcamp_live, _html} = live(conn, ~p"/en/bootcamps")
+    test "lists all bootcamps", %{conn: conn} do
+      bootcamp = bootcamp_fixture()
+
+      assert {:ok, _bootcamp_live, html} = live(conn, ~p"/en/bootcamps")
+      assert html =~ bootcamp.name
+    end
+  end
+
+  describe "GET /:locale/bootcamps/:slug" do
+    test "lists all lessons from a bootcamp", %{conn: conn} do
+      lesson = lesson_fixture()
+      bootcamp = bootcamp_fixture(lesson_names: [lesson.name])
+
+      assert {:ok, _bootcamp_live, html} = live(conn, ~p"/en/bootcamps/#{bootcamp.slug}")
+      assert html =~ bootcamp.name
+      assert html =~ lesson.description_en
     end
   end
 end
