a57e56e76da76d3b81273d9684bcf28a288cd74f
Daniel Bergholz <bergholz.daniel@gmail.com>
2024-06-03 11:47:16 -0300

    Create :mix_env env. variable in `config.exs`

--- config/config.exs
+++ config/config.exs
@@ -67,3 +67,5 @@ config :gettext, :default_locale, "en"
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
+
+config :techschool, mix_env: Mix.env()
