78b4e00ea10328b8a06c7d4e81c3795028236d0d
Daniel Bergholz <bergholz.daniel@gmail.com>
2024-07-01 15:51:33 -0300

    Upgrade Elixir to 1.17

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Techschool.MixProject do
     [
       app: :techschool,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
