19dd66a37777e936f447ce17fe35158a6cb14e44
Luiz Cezer <lccezinha@gmail.com>
2024-07-18 19:02:15 -0300

    Start creating online users counter
    - Adds a new module to hold the fn, allowing easy sharing into diff live view modules and unit tests
    - Set a Presence module and start process into application.ex

---
+++ lib/techschool_web/online_users_counter.ex
@@ -0,0 +1,33 @@
+defmodule TechschoolWeb.OnlineUsersCounter do
+  @moduledoc """
+  This module provides topics shared among the application
+  """
+
+  alias TechschoolWeb.Presence
+
+  require Logger
+
+  @online_users_topic "online_users_topic"
+
+  def track_online_user do
+    {:ok, _ref} =
+      case Presence.track(
+             self(),
+             @online_users_topic,
+             "user:#{Ecto.UUID.generate()}",
+             %{}
+           ) do
+        {:ok, ref} ->
+          {:ok, ref}
+
+        _ ->
+          Logger.warning("Not possible to track a online user")
+      end
+  end
+
+  def get_online_users_count do
+    @online_users_topic
+    |> Presence.list()
+    |> map_size()
+  end
+end
