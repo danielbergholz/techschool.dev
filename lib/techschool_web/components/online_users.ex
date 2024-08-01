70506a371a4c13682192a70e36ed4d04409e5a56
Luiz Cezer <lccezinha@gmail.com>
2024-08-01 15:01:50 -0300

    Update lib/techschool_web/components/online_users.ex
    Co-authored-by: Daniel Bergholz <bergholz.daniel@gmail.com>

--- lib/techschool_web/components/online_users.ex
+++ lib/techschool_web/components/online_users.ex
@@ -6,46 +6,24 @@ defmodule TechschoolWeb.OnlineUsers do
 
   attr :count, :integer, required: true
 
-  def show_online_users(%{count: 0} = assigns) do
-    ~H"""
-    """
-  end
-
-  def show_online_users(%{count: 1} = assigns) do
-    ~H"""
-    <span id="online_users_count" class="mt-2 relative inline-flex">
-      <button
-        type="button"
-        class="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-green bg-white dark:bg-slate-800 transition ease-in-out duration-150 cursor-not-allowed ring-1 ring-slate-900/10 dark:ring-slate-200/20"
-        disabled=""
-      >
-        <%= @count %> online user
-      </button>
-      <span class="flex absolute h-3 w-3 top-0 right-0 -mt-1 -mr-1">
-        <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green opacity-75">
-        </span>
-        <span class="relative inline-flex rounded-full h-3 w-3 bg-green"></span>
-      </span>
-    </span>
-    """
-  end
+  def show_online_users(%{count: 0} = assigns), do: ~H""
 
   def show_online_users(assigns) do
     ~H"""
-    <span id="online_users_count" class="mt-2 relative inline-flex">
-      <button
-        type="button"
-        class="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-green bg-white dark:bg-slate-800 transition ease-in-out duration-150 cursor-not-allowed ring-1 ring-slate-900/10 dark:ring-slate-200/20"
-        disabled=""
-      >
-        <%= @count %> online users
-      </button>
+    <div
+      id="online_users_count"
+      class="mt-2 relative inline-flex"
+      aria-label="Number of online users using TechSchool"
+    >
+      <p class="px-4 py-2 font-semibold text-sm rounded-md text-green bg-slate-800">
+        <%= @count %> user<%= if @count == 1, do: "", else: "s" %> online
+      </p>
       <span class="flex absolute h-3 w-3 top-0 right-0 -mt-1 -mr-1">
         <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green opacity-75">
         </span>
         <span class="relative inline-flex rounded-full h-3 w-3 bg-green"></span>
       </span>
-    </span>
+    </div>
     """
   end
 end
