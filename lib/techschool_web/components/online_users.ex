2f895f374d794e53c1dd06ebfc05ec980664297f
Daniel Bergholz <bergholz.daniel@gmail.com>
2024-08-01 15:56:36 -0300

    Add translations to show_online_users

--- lib/techschool_web/components/online_users.ex
+++ lib/techschool_web/components/online_users.ex
@@ -3,6 +3,7 @@ defmodule TechschoolWeb.OnlineUsers do
   Component to show the count of online users.
   """
   use Phoenix.Component
+  import TechschoolWeb.Gettext
 
   attr :count, :integer, required: true
 
@@ -16,7 +17,7 @@ defmodule TechschoolWeb.OnlineUsers do
       aria-label="Number of online users using TechSchool"
     >
       <p class="px-4 py-2 font-semibold text-sm rounded-md text-green bg-slate-800">
-        <%= @count %> user<%= if @count == 1, do: "", else: "s" %> online
+        <%= @count %> <%= gettext("user") %><%= if @count == 1, do: "", else: "s" %> online
       </p>
       <span class="flex absolute h-3 w-3 top-0 right-0 -mt-1 -mr-1">
         <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green opacity-75">
