4aaef79eac8cb7c41d4ede3f9f6b5d3493ff76bb
Luiz Cezer <lccezinha@gmail.com>
2024-08-01 15:02:38 -0300

    Update test/techschool_web/components/online_users_test.exs
    Co-authored-by: Daniel Bergholz <bergholz.daniel@gmail.com>

--- test/techschool_web/components/online_users_test.exs
+++ test/techschool_web/components/online_users_test.exs
@@ -27,7 +27,7 @@ defmodule TechschoolWeb.OnlineUsersTest do
       rendered_component_html = render_component(&OnlineUsers.show_online_users/1, assigns)
 
       assert rendered_component_html =~
-               ~s(<span id=\"online_users_count\" class=\"mt-2 relative inline-flex\">\n  <button type=\"button\" class=\"inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-green bg-white dark:bg-slate-800 transition ease-in-out duration-150 cursor-not-allowed ring-1 ring-slate-900/10 dark:ring-slate-200/20\" disabled=\"\">\n    2 online users\n  </button>\n  <span class=\"flex absolute h-3 w-3 top-0 right-0 -mt-1 -mr-1\">\n    <span class=\"animate-ping absolute inline-flex h-full w-full rounded-full bg-green opacity-75\">\n    </span>\n    <span class=\"relative inline-flex rounded-full h-3 w-3 bg-green\"></span>\n  </span>\n</span>)
+               ~s(<div id=\"online_users_count\" class=\"mt-2 relative inline-flex\" aria-label=\"Number of online users using TechSchool\">\n  <p class=\"px-4 py-2 font-semibold text-sm rounded-md text-green bg-slate-800\">\n    2 users online\n  </p>\n  <span class=\"flex absolute h-3 w-3 top-0 right-0 -mt-1 -mr-1\">\n    <span class=\"animate-ping absolute inline-flex h-full w-full rounded-full bg-green opacity-75\">\n    </span>\n    <span class=\"relative inline-flex rounded-full h-3 w-3 bg-green\"></span>\n  </span>\n</div>)
     end
   end
 end
