8401dff554e2cb66819b4a3b0b8b901cca738558
Daniel Bergholz <bergholz.daniel@gmail.com>
2024-10-31 13:15:48 -0300

    Fix youtube module

--- lib/techschool/youtube.ex
+++ lib/techschool/youtube.ex
@@ -6,19 +6,24 @@ defmodule Techschool.YouTube do
   @base_url "https://www.googleapis.com/youtube/v3"
   @api_key System.get_env("YOUTUBE_API_KEY")
 
-  def fetch_course(%{id: id, type: type}) do
+  def fetch_course(%{youtube_course_id: id, type: type}) do
     video_url = "#{@base_url}/#{type}s?part=snippet&key=#{@api_key}&id=#{id}"
 
-    with {:ok, data} <- Req.get(video_url) do
-      hd(data.body["items"])["snippet"]
+    case Req.get(video_url) do
+      {:ok, %{body: body}} -> hd(body["items"])["snippet"]
+      _ -> nil
     end
   end
 
   def fetch_channel(id) do
     channel_url = "#{@base_url}/channels?part=snippet&key=#{@api_key}&id=#{id}"
 
-    with {:ok, data} <- Req.get(channel_url) do
-      hd(data.body["items"])["snippet"]
+    case Req.get(channel_url) do
+      {:ok, %{body: body}} ->
+        hd(body["items"])["snippet"]
+
+      _ ->
+        nil
     end
   end
 end
